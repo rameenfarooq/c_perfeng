385c1f447b3f835041a0425b4b943f000838a594
mpsinterns <40146810+mpsinterns@users.noreply.github.com>
2018-07-13 23:11:16 -0400

    Create c_isdigit.c

---
+++ c_isdigit.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <sys/time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main(void)
+{
+    struct timeval stop, start;
+    gettimeofday(&start, NULL);
+    unsigned int count;
+    for( count=0; count <= 1000000; count++) {
+        isdigit('1234');
+    }
+   
+    gettimeofday(&stop, NULL);
+    double secs = (double)(stop.tv_usec - start.tv_usec) / 1000000 + (double)(stop.tv_sec - start.tv_sec);
+    printf("isdigit took %6.9f\n", secs);
+    
+}
