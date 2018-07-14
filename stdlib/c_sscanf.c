c46c74e4715164e6eeceb71439d929c47c232064
mpsinterns <40146810+mpsinterns@users.noreply.github.com>
2018-07-13 23:30:41 -0400

    Update c_sscanf.c

--- stdlib/c_sscanf.c
+++ stdlib/c_sscanf.c
@@ -16,6 +16,6 @@ int main(void)
    
     gettimeofday(&stop, NULL);
     double secs = (double)(stop.tv_usec - start.tv_usec) / 1000000 + (double)(stop.tv_sec - start.tv_sec);
-    printf("isdigit took %6.6f\n", secs);
+    printf("sscanf took %6.6f\n", secs);
     
 }
