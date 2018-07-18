a74b3be5ff4234c650805dba1415118b1fe987ec
rameenfarooq <rameenf14@gmail.com>
2018-07-18 00:50:34 -0400

    Create iswspace.c

---
+++ iswspace.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <sys/time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <wchar.h>
+#include <wctype.h>
+
+
+int main(void)
+{
+    struct timeval stop, start;
+    gettimeofday(&start, NULL);
+    unsigned int count;
+
+    for( count=0; count <= 1000000; count++) {
+        if(iswspace('g'))
+            return 0;
+
+  }
+
+    
+    gettimeofday(&stop, NULL);
+    double secs = (double)(stop.tv_usec - start.tv_usec) / 1000000 + (double)(stop.tv_sec - start.tv_sec);
+    printf("iswspace took %6.6f\n", secs);
+}
