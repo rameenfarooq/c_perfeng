4209560bfd9b012d34f487f53549f2d7ec26372d
rameenfarooq <rameenf14@gmail.com>
2018-07-22 22:03:04 -0400

    Create wcsrchr.c

---
+++ c_files/wcsrchr.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <sys/time.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <wchar.h>
+#include <wctype.h>
+
+
+
+int main(void)
+{
+    struct timeval stop, start;
+    gettimeofday(&start, NULL);
+    unsigned int count;
+    wchar_t arr[] = L"abcdef";
+    
+    for( count=0; count <= 1000000; count++) {
+        wcsrchr(arr, L'b');  
+    }
+    
+    gettimeofday(&stop, NULL);
+    double secs = (double)(stop.tv_usec - start.tv_usec) / 1000000 + (double)(stop.tv_sec - start.tv_sec);
+    printf("wcschr took %6.6f\n", secs);
+
+}
